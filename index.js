e645015e30507c92d34092e096c3ccb4d967eb79
danheavern <danheavern@gmail.com>
2022-04-05 16:19:14 -0400

    favicon

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/word', (req,res) => {
 
 app.use(express.static('style'))
 app.use(express.static('scripts'))
-app.use(express.static('images'))
+app.use(express.static('./images/favicon.ico'))
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
